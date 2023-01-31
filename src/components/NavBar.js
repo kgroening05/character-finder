aaecd00be0ed27e4edb47c664038b6a2f7cd2f03
Kyle Groening <kylegroening@gmail.com>
2023-01-30 21:09:21 -0500

    Add navbar component and styles

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './NavBar.css'
+
+export default function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Leaderboards</li>
+      </ul>
+    </nav>
+  );
+}
