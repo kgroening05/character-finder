b6ace2f2d26fb040c651e94c877fd0e5569bb597
Kyle Groening <kylegroening@gmail.com>
2023-01-30 22:16:21 -0500

    Add levelselect page

---
+++ src/pages/LevelSelect.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function LevelSelect() {
+
+  const levels = ['ad2222', 'robo-city', 'ultimate-space-battle','universe113']
+
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
