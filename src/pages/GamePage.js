9a7172cd04dc8332babbcd22e291bc384fd046cf
Kyle Groening <kyle.groening@cytiva.com>
2023-01-31 15:49:27 -0500

    Add GamePage.js

---
+++ src/pages/GamePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from '../components/NavBar';
+
+
+export default function GamePage(){
+    return (
+        <>
+        <NavBar />
+        <div>HelloGame</div>
+        </>
+    );
+}
