c6b2dd627711c9fbd020495460948d2a7764743a
Kyle Groening <kyle.groening@cytiva.com>
2023-01-31 14:45:04 -0500

    Remove MainImage import

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,12 +1,23 @@
 import './App.css';
-import MainImage from './MainImage'
 import NavBar from '../components/NavBar';
+import LevelSelectCard from '../components/LevelSelectCard';
+
+import ad2222 from '../assets/ad2222.jpg'
+import roboCity from '../assets/robo-city.jpg'
+import ultimateSpaceBattle from '../assets/ultimate-space-battle.jpg'
+import universe113 from '../assets/universe113.jpg'
+
+const levels = [{image: ad2222, name: "A.D. 2222"},
+                {image: roboCity, name: "Robo City"},
+                {image: ultimateSpaceBattle, name: 'Ultimate Space Battle'},
+                {image: universe113, name: 'Universe 113'},
+              ]
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <MainImage />
+      <LevelSelectCard levelsList={levels} />
     </div>
   );
 }
