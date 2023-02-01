ba78737ec451b2c47ad8a69f2851389bf76a2f45
Kyle Groening <kylegroening@gmail.com>
2023-01-31 21:28:23 -0500

    Move game level imports, object to separate file

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,23 +1,13 @@
 import './App.css';
 import NavBar from '../components/NavBar';
 import LevelSelectCard from '../components/LevelSelectCard';
-
-import ad2222 from '../assets/ad2222.jpg'
-import roboCity from '../assets/robo-city.jpg'
-import ultimateSpaceBattle from '../assets/ultimate-space-battle.jpg'
-import universe113 from '../assets/universe113.jpg'
-
-const levels = [{image: ad2222, name: "A.D. 2222"},
-                {image: roboCity, name: "Robo City"},
-                {image: ultimateSpaceBattle, name: 'Ultimate Space Battle'},
-                {image: universe113, name: 'Universe 113'},
-              ]
+import { levelList } from '../assets/levelList'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <LevelSelectCard levelsList={levels} />
+      <LevelSelectCard levelsList={levelList} />
     </div>
   );
 }
