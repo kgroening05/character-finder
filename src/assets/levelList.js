b1dc4356093b047e8a2b9179d9b2de9681b5387d
Kyle Groening <kylegroening@gmail.com>
2023-01-31 21:26:38 -0500

    Move level data to separate file

---
+++ src/assets/levelList.js
@@ -0,0 +1,16 @@
+// Image imports
+import ad2222 from './ad2222.jpg'
+import roboCity from './robo-city.jpg'
+import ultimateSpaceBattle from './ultimate-space-battle.jpg'
+import universe113 from './universe113.jpg'
+
+// Coordinate Imports
+import universe113Coords from '../assets/universe113coords.json'
+
+
+export const levelList = [
+  {image: ad2222, name: "A.D. 2222", link: 'ad2222'},
+  {image: roboCity, name: "Robo City", link: 'robocity'},
+  {image: ultimateSpaceBattle, name: "Ultimate Space Battle", link: 'spacebattle'},
+  {image: universe113, name: "Universe 113", link: 'universe113', coords: universe113Coords},
+  ]
