52f68a15c3f020831ea8f2072722737fae6f03b5
Kyle Groening <kylegroening@gmail.com>
2023-02-01 21:14:19 -0500

    Add a file to list character names and portraits

---
+++ src/assets/charRefs/universe113portraits.js
@@ -0,0 +1,12 @@
+// image imports
+import Arnold from './ArnoldShortman.webp'
+import Groot from './BabyGroot.png'
+import Courage from './Courage.webp'
+import R2D2 from './R2D2.png'
+
+export const universe113portraits = [
+    {name: "Courage", portrait: Courage},
+    {name: "R2D2" , portrait: R2D2},
+    {name: "Baby Groot" , portrait: Groot},
+    {name: "Arnold Shortman" , portrait: Arnold},
+]
