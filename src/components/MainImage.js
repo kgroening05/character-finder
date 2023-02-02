99e3d264cfee8c24afbe9e784ed10f83991c498a
Kyle Groening <kylegroening@gmail.com>
2023-02-01 21:15:32 -0500

    Move game logic to GamePage

--- src/components/MainImage.js
+++ src/components/MainImage.js
@@ -1,22 +1,7 @@
 import React from "react";
 import './MainImage.css'
-import isPointInPolygon from '../utils/isPointInPolygon'
 
-export default function MainImage({ image, coords }){
-    function handleClick(e){
-        const yAdjust = e.target.offsetTop
-        const xAdjust = e.target.offsetLeft
-        const scale = e.target.naturalWidth / e.target.width
-        const x = (e.pageX - xAdjust) * scale
-        const y = (e.pageY - yAdjust) * scale
-        //console.log({x,y})
-        for (const character in coords) {
-            if (isPointInPolygon(x,y,coords[character])){
-                console.log(`You found ${character}!`)
-            }
-        }
-    }
-
+export default function MainImage({ image, handleClick }){
     return (
         <img src={image} alt="" onClick={e => handleClick(e)} />
     )
--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import './GamePage.css'
 import { useParams } from "react-router-dom";
 import { levelList } from "../assets/levelList";
 import MainImage from "../components/MainImage";
+import CharacterTarget from "../components/CharacterTarget";
+import isPointInPolygon from '../utils/isPointInPolygon'
+
 
 function matchLevel(level, levelList) {
   for (const element of levelList) {
@@ -13,15 +16,51 @@ function matchLevel(level, levelList) {
   return null;
 }
 
+function generateUnusedIndex(usedNumberList, fullList){
+  const newNum = Math.floor(Math.random() * fullList.length)
+  const isFound = usedNumberList.find(element=>element === newNum)
+  return (isFound ? generateUnusedIndex(usedNumberList, fullList) : newNum)
+}
+
+
+
 export default function GamePage() {
   const { level } = useParams();
   const levelObj = matchLevel(level, levelList)
 
+  const randomStart = Math.floor(Math.random()* levelObj.portraits.length)
+  const [currentCharIndex, setCurrentCharIndex] = useState([randomStart])
+
+  function handleClick(e){
+    const yAdjust = e.target.offsetTop
+    const xAdjust = e.target.offsetLeft
+    const scale = e.target.naturalWidth / e.target.width
+    const x = (e.pageX - xAdjust) * scale
+    const y = (e.pageY - yAdjust) * scale
+    // console.log({x,y})
+    const coords = levelObj.coords
+    for (const character in coords) {
+      if (isPointInPolygon(x,y,coords[character])){
+        setCurrentCharIndex([...currentCharIndex, generateUnusedIndex(currentCharIndex, levelObj.portraits)])
+        alert(`You found ${character}!`)
+        checkWinner()
+      }
+    }
+  }
+
+  function checkWinner(){
+    if (currentCharIndex.length > levelObj.portraits.length - 1) {
+      alert('You found them all!')
+    }
+
+  }
+
   return (
     <>
+      <CharacterTarget charList={levelObj.portraits} currentCharIndex={currentCharIndex[currentCharIndex.length - 1]} />
       <div id="image-container">
-        <div>HelloGame {levelObj.name}</div>
-        <MainImage image={levelObj.image} coords={levelObj.coords} />
+        <div>Level: {levelObj.name} currentCharIndex: {currentCharIndex}</div>
+        <MainImage image={levelObj.image} handleClick={handleClick}/>
       </div>
     </>
   );
