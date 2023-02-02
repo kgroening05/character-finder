66f2e0972ffd1960ac6dcacf838bfd64ff4667df
Kyle Groening <kylegroening@gmail.com>
2023-02-01 22:27:29 -0500

    Add additional character coords and portraits

--- src/assets/charRefs/universe113portraits.js
+++ src/assets/charRefs/universe113portraits.js
@@ -3,10 +3,24 @@ import Arnold from './ArnoldShortman.webp'
 import Groot from './BabyGroot.png'
 import Courage from './Courage.webp'
 import R2D2 from './R2D2.png'
+import Waldo from './Waldo.png'
+import Totoro from './Totoro.png'
+import EarthwormJim from './EarthwormJim.png'
+import Roger from './RogerSmith.png'
+import BigBoss from './BigBoss.png'
+import Gandalf from './Gandalf.png'
+import Fry from './PhilipFry.png'
 
 export const universe113portraits = [
     {name: "Courage", portrait: Courage},
     {name: "R2D2" , portrait: R2D2},
     {name: "Baby Groot" , portrait: Groot},
     {name: "Arnold Shortman" , portrait: Arnold},
+    {name: "Waldo" , portrait: Waldo},
+    {name: "Totoro" , portrait: Totoro},
+    {name: "Earthworm Jim" , portrait: EarthwormJim},
+    {name: "Roger Smith" , portrait: Roger},
+    {name: "Big Boss" , portrait: BigBoss},
+    {name: "Galdalf" , portrait: Gandalf},
+    {name: "Philip J. Fry" , portrait: Fry}
 ]
