3ae04faec7cbe16e14a75cd69340fe8682128880
Kyle Groening <kylegroening@gmail.com>
2023-02-03 18:26:57 -0500

    Fix import errors and level name error

--- src/pages/LevelSelect.js
+++ src/pages/LevelSelect.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import LevelSelectCard from "./components/LevelSelectCard";
+import LevelSelectCard from "../components/LevelSelectCard";
 import './LevelSelect.css'
 
 export default function LevelSelect({ levelsList }){
   const levels = Object.keys(levelsList)
-  console.log({ levels })
   return (
     <div id="level-cards">
       {levels.map((level, index)=>{     
+        const currentLevel = levelsList[level]
         return (
           <Link key={index} to={`game/${level}`}>
             <div className="level">
               <LevelSelectCard level={level} levelsList={levelsList}/>
-              <p>{level.name}</p>
+              <p>{currentLevel.name}</p>
             </div>
           </Link>
         ); 
