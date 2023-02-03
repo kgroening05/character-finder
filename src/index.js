cb744e9228a485ad54a2fe5ee910bb14dbffe4c4
Kyle Groening <kylegroening@gmail.com>
2023-02-03 17:32:11 -0500

    Move firebase concerns to separate file

--- src/index.js
+++ src/index.js
@@ -6,31 +6,9 @@ import App from './pages/App';
 import ErrorPage from './pages/ErrorPage';
 import Leaderboards from './pages/Leaderboards';
 import GamePage from './pages/GamePage'
-import LevelSelectCard from './components/LevelSelectCard';
-import { levelList } from './assets/levelList'
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import LevelSelect from './components/LevelSelect';
+import gamedata from './assets/gamedata.json'
 
-import { getStorage, ref } from "firebase/storage";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDnBLoenB15oBnkzMMcdKxBbyvq3ZB6BPU",
-  authDomain: "character-finder-5efb0.firebaseapp.com",
-  projectId: "character-finder-5efb0",
-  storageBucket: "character-finder-5efb0.appspot.com",
-  messagingSenderId: "68287017393",
-  appId: "1:68287017393:web:de6842b3186fbb0ab20d90"
-};
-
-// Create a reference with an initial file path and name
-const app = initializeApp(firebaseConfig);
-const storage = getStorage(app);
-const pathReference = ref(storage, 'portraits/ArnoldShortman.webp');
-console.log(pathReference)
-
-// Initialize Firebase
 
 const router = createBrowserRouter([
   {
@@ -40,7 +18,7 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <LevelSelectCard levelsList={levelList} />,
+        element: <LevelSelect levelsList={gamedata.levels} />,
       },
       {
         path: "/leaderboards",
@@ -54,7 +32,6 @@ const router = createBrowserRouter([
   },
 
 ]);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
