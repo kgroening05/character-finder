c838ba59961876448f331753e555cb650bfba485
Kyle Groening <kylegroening@gmail.com>
2023-02-03 17:31:39 -0500

    Add module to handle firebase concerns

---
+++ src/firebase.js
@@ -0,0 +1,35 @@
+import { initializeApp } from "firebase/app";
+import { getStorage, ref as refStorage, getDownloadURL } from "firebase/storage";
+import { getDatabase, ref as refDB , onValue } from "firebase/database";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDnBLoenB15oBnkzMMcdKxBbyvq3ZB6BPU",
+  authDomain: "character-finder-5efb0.firebaseapp.com",
+  projectId: "character-finder-5efb0",
+  storageBucket: "character-finder-5efb0.appspot.com",
+  databaseURL: "https://character-finder-5efb0-default-rtdb.firebaseio.com",
+  messagingSenderId: "68287017393",
+  appId: "1:68287017393:web:de6842b3186fbb0ab20d90"
+};
+
+// Create a reference with an initial file path and name
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+const database = getDatabase(app);
+
+export function loadFirebaseStorageImage(path, stateCallback){
+    const pathReference = refStorage(storage, path);
+    getDownloadURL(pathReference).then((url)=>{
+        stateCallback(url)
+    })
+}
+
+export function loadFireBaseDatabase () {
+  const dbReference = refDB(database, '/levels/')
+  let data
+  onValue(dbReference, snapshot =>{
+    data = snapshot.val()
+  }, {onlyOnce: true});
+  return data;
+}
