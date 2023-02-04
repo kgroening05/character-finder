d63fc6cb66da2cab9c6746498b481846a4c0f43b
Kyle Groening <kylegroening@gmail.com>
2023-02-04 10:49:14 -0500

    Add brightness control

--- src/components/MainImage.js
+++ src/components/MainImage.js
@@ -2,10 +2,13 @@ import React, { useState } from "react";
 import './MainImage.css'
 import { loadFirebaseStorageImage } from "../firebase";
 
-export default function MainImage({ image, handleClick }){
+export default function MainImage({ image, handleClick, brightness }){
     const [mainImageUrl, setMainImageUrl] = useState(null)
     loadFirebaseStorageImage(image, setMainImageUrl)
+    const style = {
+        filter: `brightness(${brightness})`,
+    }
     return (
-        <img src={mainImageUrl} alt="" onClick={e => handleClick(e)} />
+        <img src={mainImageUrl} alt="" onClick={e => handleClick(e)} style={style} />
     )
 }
