1059e8c6f5b7eb9bcbc8d255e55fbf43efcc70d3
Kyle Groening <kylegroening@gmail.com>
2023-02-05 21:49:28 -0500

    Add state and leaderboard data functions

--- src/pages/Leaderboards.js
+++ src/pages/Leaderboards.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { getLeaderBoardData } from "../firebase";
+import LevelLeaderboard from "../components/LevelLeaderboard";
 
-export default function Leaderboards() {
+export default function Leaderboards({ levelsList }) {
+  const levels = Object.keys(levelsList)
+  const [leaderboardData, setLeaderboardData] = useState()
+
+  getLeaderBoardData(levels[3], appendLeaderboardData)
+  
+  function appendLeaderboardData(newData){
+    setLeaderboardData(newData)
+  }
+
   return (
     <>
-    <div>Hello</div>
+      <div>Hello</div>
+      <LevelLeaderboard leaderboardData={leaderboardData}/>
     </>
   )
 }
