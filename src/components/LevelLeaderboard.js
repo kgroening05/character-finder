a507523311e874d85c32d5c94ff85fc985ca7243
Kyle Groening <kylegroening@gmail.com>
2023-02-05 21:48:42 -0500

    Add new component for populating leaderboards

---
+++ src/components/LevelLeaderboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function LevelLeaderboard ({ leaderboardData }) {
+  console.log(leaderboardData[0])
+
+  return (
+    <div id="level" className="level-leaderboard">
+      <ul>
+        <li>hello</li>
+      </ul>
+    </div>       
+  )
+}
