52e9bfe5cb6111ee557839af64675030f16491fe
Kyle Groening <kylegroening@gmail.com>
2023-02-06 22:04:26 -0500

    Add useState and useEffect, refactor to table

--- src/components/LevelLeaderboard.js
+++ src/components/LevelLeaderboard.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getLeaderBoardData } from "../firebase";
 
-export default function LevelLeaderboard ({ leaderboardData }) {
-  console.log(leaderboardData[0])
+
+export default function LevelLeaderboard ({ level, name }) {
+  const [leaderboardData, setLeaderboardData] = useState(null)
+
+  useEffect(()=>{
+    getLeaderBoardData(level)
+    .then((data)=>{
+      console.log(data)
+      setLeaderboardData(data)
+    })
+  },[level]);
 
   return (
-    <div id="level" className="level-leaderboard">
-      <ul>
-        <li>hello</li>
-      </ul>
-    </div>       
+    <table className="level-leaderboard">
+      <th colSpan="2" className="table-title">{name}</th>
+      {leaderboardData 
+      ? leaderboardData.map( element => {
+        return (
+          <tr className="board-item">
+            <td className="player-name">{element.name}</td>
+            <td className="player-score">{element.score}</td>
+          </tr>
+        )
+      }) 
+      : null
+      }
+    </table>       
   )
 }
